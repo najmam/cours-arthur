f6220a9e1e7a0af5898edc42307b445b50fcccfc
Super Jesus <john.l3mon@gmail.com>
2023-11-13 17:08:36 +0100

    exo 2
    DONE !

--- root.gd
+++ root.gd
@@ -1,5 +1,7 @@
 extends Node2D	
 
+var axisx = true
+var axisy = false
 var islooped = true
 var width = ProjectSettings.get_setting("display/window/size/viewport_width")
 var height = ProjectSettings.get_setting("display/window/size/viewport_height")
@@ -7,32 +9,43 @@ var height = ProjectSettings.get_setting("display/window/size/viewport_height")
 
 func _ready():
 
-
 	$player.position.x = width/2
 	$player.position.y = height/2
-	
-	
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 
 	if islooped == true:
 		if  $player.position.y >= height/10:
-			$player.position.y = $player.position.y -1
+			$player.position.y = $player.position.y -2
 		if  $player.position.y <= height/10 && $player.position.x <= width/18*17:
-			$player.position.x = $player.position.x +2
-			#At the end of first path, ignore what's up there ^
+			$player.position.x = $player.position.x +4
+	#At the end of first path, ignore what's up there ^
 		if  $player.position.x >= width/18*17:
 			islooped = false
-			print("isokay")
-#
+			print("islooped is now false")
+
 	if islooped == false:
-		if  $player.position.x <= width/18*17:
-			$player.position.x = $player.position.x +1
-		if  $player.position.x >= width/18:
-			$player.position.x = $player.position.x -1
-		if  $player.position.y >= height/10:
-			$player.position.y = $player.position.y -1
-		if  $player.position.y <= height/10*9:
-			$player.position.y = $player.position.y +1
+		# Close to the edge
+		if  $player.position.x >= width/18*17:
+			axisx = false
+			print("go left")
+		if  $player.position.x <= width/18:
+			axisx = true
+			print("go right")
+		if  $player.position.y >= height/10*9:
+			axisy = false
+			print("go up")
+		if  $player.position.y <= height/10:
+			axisy = true
+			print("go down")
+		
+		
+		if  axisx == true:
+			$player.position.x = $player.position.x +2
+		if  axisx == false:
+			$player.position.x = $player.position.x -2
+		if  axisy == true:
+			$player.position.y = $player.position.y +2
+		if  axisy == false:
+			$player.position.y = $player.position.y -2
